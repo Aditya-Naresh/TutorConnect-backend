40995444cc39720c6c5e2b8b179a479862d8bf7e
Aditya Naresh <adityanaresh007@gmail.com>
2024-06-29 15:46:30 +0530

    Approve Tutor Completed

--- useradmin/serializers.py
+++ useradmin/serializers.py
@@ -1,8 +1,20 @@
 from rest_framework import serializers
-from accounts.models import User
+from accounts.models import User, Subject, Certification
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['first_name', 'last_name', 'email', 'id', 'is_blocked', 'is_active', 'auth_provider']
+        fields = ['first_name', 'last_name', 'email', 'id', 'is_blocked', 'is_active', 'auth_provider', 'is_approved', 'rate']
+
+
+class SubjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Subject
+        fields = '__all__'
+
+
+class CertificationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Certification
+        fields = '__all__'
