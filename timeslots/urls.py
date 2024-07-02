b8beb993abb4d05da15b7ea7f622e72a2333d88a
Aditya Naresh <adityanaresh007@gmail.com>
2024-07-02 13:13:53 +0530

    Requests, Notifications and booking time slots complete pending to add user Dashboards

--- timeslots/urls.py
+++ timeslots/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import CreateTutorDatesView, TutorDatesListView, TimeSlotsListView
+from .views import CreateTutorDatesView, TutorDatesListView, TimeSlotsListView, TutorListView, TuitionRequestListCreateView, TuitionRequestRetrieveUpdateView, TimeSlotRetrieveUpdateView
 
 urlpatterns = [
     path('create-tutor-dates/', CreateTutorDatesView.as_view(), name='create-tutor-dates'),
     path('available-dates/<id>', TutorDatesListView.as_view(), name='tutor-dates'),
     path('timeSlots/<id>', TimeSlotsListView.as_view(), name='time-slots'),
+    path('BooktimeSlots/<id>', TimeSlotRetrieveUpdateView.as_view(), name='time-slots-booking'),
+    path('tutor-list/', TutorListView.as_view(), name='tutor-list'),
+    path('tuition-request/', TuitionRequestListCreateView.as_view(), name='tuition-request'),
+    path('tuition-request/<id>', TuitionRequestRetrieveUpdateView.as_view(), name='tuition-request-id'),
 ]
