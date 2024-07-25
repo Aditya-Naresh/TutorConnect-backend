63676402912f2c378f3b3f5fce54547961974033
Aditya Naresh <adityanaresh007@gmail.com>
2024-07-25 18:10:55 +0530

    Using the old models for the tuition request and certifications

--- timeslots/permissions.py
+++ timeslots/permissions.py
@@ -1,15 +1,12 @@
 from rest_framework.permissions import BasePermission
-from . models import User
+from accounts.models import User
 
 
 
 class TutorPermission(BasePermission):
     def has_permission(self, request, view):
-        return request.user.is_authenticated and request.user.role == User.Role.TUTOR
-    
-
+        return bool(request.user and request.user.is_authenticated and request.user.role == User.Role.TUTOR)
 
 class StudentPermission(BasePermission):
     def has_permission(self, request, view):
-        return request.user.is_authenticated and request.user.role == User.Role.STUDENT
-    
+        return bool(request.user and request.user.is_authenticated and request.user.role == User.Role.STUDENT)
