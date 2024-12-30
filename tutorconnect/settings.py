874babde7ab79a3fbca07c150a3535725890cc5e
Aditya Naresh <adityanaresh007@gmail.com>
2024-12-30 22:26:49 +0530

    Postgres added

--- tutorconnect/settings.py
+++ tutorconnect/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
-ALLOWED_HOSTS = ["tutorconnect.codewithjoe.in","4.240.107.99"]
+ALLOWED_HOSTS = ["tutorconnect.codewithjoe.in", "4.240.107.99"]
 
 
 # Application definition
@@ -87,13 +87,24 @@ CHANNEL_LAYERS = {
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "tutorconnect_db_admin",
+        "PASSWORD": "Pass1234$",
+        "HOST": "tutor-connectdb.postgres.database.azure.com",
+        "PORT": "5432",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
